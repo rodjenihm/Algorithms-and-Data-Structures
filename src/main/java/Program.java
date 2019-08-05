9d91327fba4db09b952a979b88fb622d29822983
Данијел Митровић <rodjenihm@outlook.com>
2019-08-05 03:20:46 +0200

    Update List: Impemented add and size methods

---
+++ src/main/java/Program.java
@@ -0,0 +1,18 @@
+import datastructures.List;
+
+import java.util.Iterator;
+
+public class Program {
+    public static void main(String[] args) {
+        List<Integer> list = new List<Integer>();
+        list.add(3);
+        list.add(5);
+        list.add(7);
+        list.add(6);
+        list.add(1);
+
+        for (Integer integer : list) {
+            System.out.println(integer);
+        }
+    }
+}
--- src/main/java/datastructures/List.java
+++ src/main/java/datastructures/List.java
@@ -1,4 +1,59 @@
 package datastructures;
 
-public class List {
+import java.util.Iterator;
+
+public class List<Type> implements Iterable<Type> {
+    private Node root = null;
+    private Node tail = null;
+    private int size = 0;
+
+    public int size() {
+        return size;
+    }
+
+    public void add(Type value) {
+        Node node = new Node();
+        node.value = value;
+        node.next = null;
+
+        if (root != null) {
+            tail.next = node;
+            tail = node;
+        } else {
+            root = node;
+            tail = node;
+        }
+        size++;
+    }
+
+    public Iterator<Type> iterator() {
+        return new ListIterator(root);
+    }
+
+    class ListIterator implements Iterator<Type>{
+        Node current;
+
+        ListIterator (Node root) {
+            this.current = root;
+        }
+
+        public boolean hasNext() {
+            return  current != null;
+        }
+
+        public Type next() {
+            Type value = current.value;
+            current = current.next;
+            return value;
+        }
+
+        public void remove() {
+            throw new UnsupportedOperationException();
+        }
+    }
+
+    class Node {
+        Type value;
+        Node next;
+    }
 }
