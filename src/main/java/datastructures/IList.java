f187d436182c7ea326ab4f3c4d4531afb430840a
Данијел Митровић <rodjenihm@outlook.com>
2019-08-14 06:44:47 +0200

    Update IList: Added get method.

--- src/main/java/datastructures/IList.java
+++ src/main/java/datastructures/IList.java
@@ -1,9 +1,8 @@
 package datastructures;
 
 public interface IList<E> extends ICollection<E> {
-    void addFirst(E item);
-    void addLast(E item);
     int indexOf(Object o);
     int lastIndexOf(Object o);
     Object[] toArray();
+    E get(int index);
 }
