c3640d4062e5f1a990980eda64ca76c7aff61cff
Данијел Митровић <rodjenihm@outlook.com>
2019-08-16 05:06:06 +0200

    Update IList: Removed method toArray.

--- src/main/java/datastructures/IList.java
+++ src/main/java/datastructures/IList.java
@@ -3,6 +3,5 @@ package datastructures;
 public interface IList<E> extends ICollection<E> {
     int indexOf(Object o);
     int lastIndexOf(Object o);
-    Object[] toArray();
     E get(int index);
 }
