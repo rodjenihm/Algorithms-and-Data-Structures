48cf790c49329367dc1cc95b9fcb602b975d3285
Данијел Митровић <rodjenihm@outlook.com>
2019-08-23 16:30:07 +0200

    Update IListIterator: Added hasPrevious and previous methods.

--- src/main/java/datastructures/interfaces/IListIterator.java
+++ src/main/java/datastructures/interfaces/IListIterator.java
@@ -3,6 +3,6 @@ package datastructures.interfaces;
 import java.util.Iterator;
 
 public interface IListIterator<E> extends Iterator<E> {
-    //boolean hasPrevious();
-    //E previous();
+    boolean hasPrevious();
+    E previous();
 }
