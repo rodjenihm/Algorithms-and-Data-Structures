b642a71f591aefd6bda2a8129d4222bec53511aa
Данијел Митровић <rodjenihm@outlook.com>
2019-08-23 17:47:05 +0200

    Update IList: Added set method.

--- src/main/java/datastructures/interfaces/IList.java
+++ src/main/java/datastructures/interfaces/IList.java
@@ -4,6 +4,7 @@ public interface IList<E> extends ICollection<E> {
     int indexOf(Object o);
     int lastIndexOf(Object o);
     E get(int index);
+    E set(int index, E element);
     IListIterator<E> listIterator();
     IListIterator<E> listIterator(int index);
 }
