421f4abbd08312c8d2fb7e47f722ff2dab962362
Данијел Митровић <rodjenihm@outlook.com>
2019-08-25 17:53:53 +0200

    Update IList: Added remove method.

--- src/main/java/datastructures/interfaces/IList.java
+++ src/main/java/datastructures/interfaces/IList.java
@@ -5,6 +5,7 @@ public interface IList<E> extends ICollection<E> {
     int lastIndexOf(Object o);
     E get(int index);
     E set(int index, E element);
+    E remove(int index);
     IListIterator<E> listIterator();
     IListIterator<E> listIterator(int index);
 }
